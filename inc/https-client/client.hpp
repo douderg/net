93cb27a1468eb895eae9517cfc2f526e61968d48
Doukas Dergiades <douderg@gmail.com>
2021-05-02 11:31:46 +0300

    added default constructor for connection

--- inc/https-client/client.hpp
+++ inc/https-client/client.hpp
@@ -4,6 +4,7 @@
 #include <boost/beast/http.hpp>
 #include <boost/beast/ssl.hpp>
 #include <boost/beast/version.hpp>
+#include <memory>
 
 namespace https {
 
@@ -12,19 +13,20 @@ typedef boost::beast::http::request<boost::beast::http::string_body> request_t;
 
 class connection {
 public:
+    connection() = default;
     connection(boost::asio::io_context& io_ctx, boost::asio::ssl::context& ssl_ctx, const std::string& sni_hostname, boost::asio::ip::tcp::resolver::results_type resolve);
     ~connection();
     connection(const connection&) = delete;
-    connection(connection&&) = default;
+    connection(connection&&);
     connection& operator=(const connection&) = delete;
-    connection& operator=(connection&&) = default;
+    connection& operator=(connection&&);
 
     response_t send(const request_t& request);
 
     void close();
 
 private:
-    boost::beast::ssl_stream<boost::beast::tcp_stream> stream_;
+    std::unique_ptr<boost::beast::ssl_stream<boost::beast::tcp_stream>> stream_;
 };
 
 class client {
--- src/client.cpp
+++ src/client.cpp
@@ -4,40 +4,51 @@
 namespace https {
 
 connection::connection(
-    boost::asio::io_context& io_ctx, 
-    boost::asio::ssl::context& ssl_ctx, 
-    const std::string& sni_hostname, 
-    boost::asio::ip::tcp::resolver::results_type resolved):
-        stream_(io_ctx, ssl_ctx) {
-    if (!SSL_set_tlsext_host_name(stream_.native_handle(), sni_hostname.c_str())) {
+        boost::asio::io_context& io_ctx, 
+        boost::asio::ssl::context& ssl_ctx, 
+        const std::string& sni_hostname, 
+        boost::asio::ip::tcp::resolver::results_type resolved) {
+    
+    stream_ = std::make_unique<boost::beast::ssl_stream<boost::beast::tcp_stream>>(io_ctx, ssl_ctx);
+    if (!SSL_set_tlsext_host_name(stream_->native_handle(), sni_hostname.c_str())) {
         throw std::runtime_error("failed setting SNI host name");
     }
-    boost::beast::get_lowest_layer(stream_).connect(resolved);
-    stream_.handshake(boost::asio::ssl::stream_base::client);
+    boost::beast::get_lowest_layer(*stream_).connect(resolved);
+    stream_->handshake(boost::asio::ssl::stream_base::client);
 }
 
 connection::~connection() {
     try {
         close();
-    } catch (const std::runtime_error err) {
+    } catch (const std::runtime_error& err) {
     }
 }
 
+connection::connection(connection&& other): stream_{other.stream_.release()} {
+    
+}
+
+connection& connection::operator=(connection&& other) {
+    stream_.reset(other.stream_.release());
+    return *this;
+}
+
 response_t connection::send(const request_t& request) {
-    boost::beast::http::write(stream_, request);
+    if (!stream_) {
+        throw std::runtime_error("connection is closed");
+    }
+    boost::beast::http::write(*stream_, request);
     boost::beast::flat_buffer buffer;
     response_t result;
-    boost::beast::http::read(stream_, buffer, result);
+    boost::beast::http::read(*stream_, buffer, result);
     return result;
 }
 
 void connection::close() {
-    boost::beast::error_code err;
-    stream_.shutdown(err);
-    if (err != boost::asio::error::eof) {
-        throw std::runtime_error("failed to shutdown connection");
+    if (stream_) {
+        stream_->shutdown();
+        stream_.reset(nullptr);
     }
-    err = {};
 }
 
 client::client():
